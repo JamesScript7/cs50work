ec9cb1e13c72c02c43b693912b8fcb6aeed71fd1
James Kim <yameskim@gmail.com>
2018-02-02 16:35:29 -0500

    Complete conditionals.c

---
+++ week-1/conditionals.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	// Prompt user for x and y
+	int x = get_int("x: ");
+	int y = get_int("y: ");
+
+	// Check x and y
+	if (x > y)
+	{
+		printf("%i is greater than %i\n", x, y);
+	}
+	else if (x < y)
+	{
+		printf("%i is less than %i\n", x, y);
+	}
+	else
+	{
+		printf("%i is equal to %i\n", x ,y);
+	}
+}
