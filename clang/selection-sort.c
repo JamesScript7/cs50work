3445343398407ee870bb0645e415de2cb05e2c73
James Kim <yameskim@gmail.com>
2018-05-01 22:20:35 -0400

    Add selection-sort algorithm

---
+++ clang/selection-sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j, k;
+	// The unsorted array
+	int arr[7] = {3,1,6,4,2,5,7};
+	int len = 7;
+
+	for (i = 0; i < len; i++)
+	{
+		int smallest = 8;
+
+		for (j = i; j < len; j++)
+		{
+			int swapped;
+			if (arr[j] < smallest)
+			{
+				smallest = arr[j];
+				swapped = j;
+			}
+			if (j == len - 1)
+			{
+				arr[swapped] = arr[i];
+				arr[i] = smallest;
+			}
+		}
+	}
+
+	printf("Array has been selection sorted!\n");
+
+	printf("{");
+
+	for (k = 0; k < len; k++)
+	{
+		if (k == len - 1)
+		{
+			printf("%i}\n", arr[k]);
+		}
+		else
+		{
+			printf("%i, ", arr[k]);
+		}
+	}
+
+	return 0;
+}
